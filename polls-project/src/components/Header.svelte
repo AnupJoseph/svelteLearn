5f099e384315bbb573c61ae0fb1937ed518d916c
AnupJoseph <anup20joseph@gmail.com>
2021-06-25 10:55:02 +0530

    Started polls project

---
+++ polls-project/src/components/Header.svelte
@@ -0,0 +1,20 @@
+<header>
+  <h1>
+    <img src="/images/logo.png" alt="Fire Art works" />
+  </h1>
+</header>
+
+<style>
+    header{
+        background: #f7f7f7;
+        padding: 20px;
+    }
+    h1{
+        margin: 0;
+        text-align: center;
+    }
+    img{
+        max-width: 180px;
+        max-height: 140px;
+    }
+</style>
