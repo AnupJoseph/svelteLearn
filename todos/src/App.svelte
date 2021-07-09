0e4f7dcee6f2e9b298f54f0b8f4b34782de944d0
AnupJoseph <anup20joseph@gmail.com>
2021-07-09 18:16:13 +0530

    ✔ Started on todo list mini-project

---
+++ todos/src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Icon from "svelte-awesome";
+	import { adjust } from "svelte-awesome/icons";
+</script>
+
+<main>
+	<h1>Todos App</h1>
+	<!-- <i class="fas fa-adjust"></i> -->
+	<Icon data={adjust} spin label="Adjust icon"></Icon>
+</main>
+
+<style>
+	h1{
+		font-family:"Poppins";
+		font-weight: bold;
+	}
+	
+</style>
