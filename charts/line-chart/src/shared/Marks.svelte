19399d3a178c7a2002650a0c43a3f5f678df9ed7
AnupJoseph <anup20joseph@gmail.com>
2021-10-10 18:24:50 +0530

    ✅ Finished the scatter plot

---
+++ charts/line-chart/src/shared/Marks.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { fly } from "svelte/transition";
+  export let dataset;
+  export let xScale;
+  export let yScale;
+</script>
+
+{#each dataset as data, i}
+  <circle
+    cx={xScale(data.sepal_length)}
+    cy={yScale(data.sepal_width)}
+    r="10"
+    in:fly={{ duration: 100, delay: 200 * i }}
+  />
+{/each}
+
+<style>
+  circle {
+    fill: #137880;
+  }
+</style>
