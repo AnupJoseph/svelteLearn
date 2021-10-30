556e06b8d61fcd81dc4df6c6b7886ca6571f8b13
AnupJoseph <anup20joseph@gmail.com>
2021-10-30 19:33:18 +0530

    🗺  Started on maps

---
+++ charts/world-map/src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+  // D3 stuff
+  import { json } from "d3";
+
+  // Components
+  import Marks from "./shared/Marks.svelte";
+
+  // Load dataset
+  let dataset = [];
+  json("https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json").then(
+    (data) => {
+      const { countries } = data.objects;
+      console.log(topojson.feature(data,countries))
+      dataset = topojson.feature(data, countries);
+    }
+  );
+
+  // Actual graph work
+  const width = 1200,
+    height = 600;
+</script>
+
+<main>
+  {console.log(dataset)}
+  <svg {width} {height}>
+    <Marks {dataset} />
+  </svg>
+</main>
+
+<style>
+  svg {
+    display: block;
+    position: absolute;
+  }
+</style>
---
+++ charts/world-map/src/shared/Marks.svelte
@@ -0,0 +1,36 @@
+<script>
+  import { draw } from "svelte/transition";
+  import { curveNatural, line, geoPath, geoEqualEarth } from "d3";
+  export let dataset;
+
+  const projection = geoEqualEarth();
+  const path = geoPath(projection);
+</script>
+
+{console.log(dataset)}
+{#each dataset.features as data}
+  <path d={path(data)} />
+{/each}
+
+<style>
+  circle {
+    fill: #137880;
+  }
+  path {
+    fill: transparent;
+    stroke: rgb(8, 250, 137);
+    stroke-width: 2.5;
+    stroke-linejoin: round;
+    stroke-dasharray: 4400;
+    stroke-dashoffset: 0;
+    animation: dash 8.5s ease-in-out;
+  }
+  @keyframes dash {
+    from {
+      stroke-dashoffset: 4400;
+    }
+    to {
+      stroke-dashoffset: 0;
+    }
+  }
+</style>
